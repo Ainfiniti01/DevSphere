8a3f1a0cf92b6bdab06750ce1ccaa9bedd5acc79
[dyad] <abdulazeezadam09@gmail.com>
2026-07-13 00:27:21 +0100

    Updated supabase/functions/project-manager/index.ts

--- supabase/functions/project-manager/index.ts
+++ supabase/functions/project-manager/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0'
-import postgres from "https://esm.sh/postgres@3.4.4"
+import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js"
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
